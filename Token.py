4aac4eb4dcd9d2d4185a1522445d6f6028c9162e
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2017-03-22 19:10:46 +0100

    Implemented Tokenizer

---
+++ Token.py
@@ -0,0 +1,30 @@
+import collections
+
+
+# noinspection PyClassHasNoInit
+class Token(collections.namedtuple('Token', ['type', 'value', 'line', 'column'])):
+    __slots__ = ()
+
+    def __str__(self):
+        s = 'Token{type='
+        s += self.prepare_string(str(self.type), 16)
+        s += ' value='
+        s += self.prepare_string(str(self.value), 16)
+        s += ' line='
+        s += self.prepare_string(str(self.line), 4)
+        s += ' column='
+        s += '\'' + str(self.column) + '\''
+        s += '}'
+        return s
+
+    @staticmethod
+    def prepare_string(s, max):
+        l = len(s)
+        if l > max:
+            s = s[0:max - 3] + '...'
+        elif l == max:
+            s = s
+        else:
+            s += '\',' + ' ' * (max - l)
+            return '\'' + s
+        return '\'' + s + '\','
---
+++ Tokenizer.py
@@ -0,0 +1,61 @@
+import re
+
+from Token import Token
+
+
+class Tokenizer:
+    input_string = ''
+    tokens_definitions = {
+        ('VAR_NAME', r'\@[\w\-]+'),
+        ('CONSTANT', r'(?:\#[a-fA-F\0-9]+)|\d*\.?\d+(?:\s*\%|(?:px))?'),
+        ('CLASS', r'\.[a-zA-Z\-]+'),
+        ('STRING', r'[a-zA-Z\-]+'),
+        ('SEMICOLON', r'\;'),
+        ('COLON', r'\:'),
+        ('COMMA', r'\,'),
+        ('BRACKET_OP', r'\('),
+        ('BRACKET_CLOSE', r'\)'),
+        ('BLOCK_OP', r'\{'),
+        ('BLOCK_CLOSE', r'\}'),
+        ('NEWLINE', r'\n'),
+        ('WHITESPACES', r'[\ \t]+'),
+        ('COMMENT', r'\/\/.*'),
+        ('COMMENT_BLOCK', r'\/\*[\s\S]*?\*\/'),
+        ('KEYWORD', r'(?:!important)|(?:all)')
+    }
+    tokens = []
+
+    def __init__(self, input_content, file=True):
+        if file:
+            with open(input_content, 'r') as f:
+                self.input_string = f.read()
+        else:
+            self.input_string = input_content
+
+    def tokenize(self):
+        for token in self.tokenizer():
+            self.tokens.append(token)
+
+    def tokenizer(self):
+        tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in self.tokens_definitions)
+        get_token = re.compile(tok_regex).match
+        match = get_token(self.input_string)
+        line_number = 1
+        current_position = 0
+        line_start = 0
+        while match is not None:
+            t = match.lastgroup
+            if t == 'NEWLINE':
+                line_start = current_position
+                line_number += 1
+            elif t == 'COMMENT_BLOCK':
+                line_number += match.group(t).count('\n')
+            elif t != 'WHITESPACES' and t != 'COMMENT':
+                yield Token(t, match.group(t), line_number, match.start() - line_start)
+            current_position = match.end()
+            match = get_token(self.input_string, current_position)
+        if current_position != len(self.input_string):
+            raise RuntimeError(
+                'Error: Unexpected character %r on line %d' % (self.input_string[current_position], line_number)
+            )
+        yield Token('EOF', '', line_number, current_position - line_start)
